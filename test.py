cc87ceba96598ed54bca71fc041c1e958b1f61e5
Dysio <td.dyszewski@gmail.com>
2020-07-28 22:32:05 +0200

    deleted test file

--- test.py
+++
@@ -1,9 +0,0 @@
-def test(*args):
-    table_arg = []
-    for arg in args:
-        table_arg += [arg]
-    return table_arg
-
-if __name__ == '__main__':
-    table = test('slowo','jest','w','tabeli')
-    print(table)
