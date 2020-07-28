4a01419f33bfc5a57952f2fa6e8afb80ffb67e6c
Dysio <td.dyszewski@gmail.com>
2020-07-28 20:39:14 +0200

    Initial commit

---
+++ book/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views as bookviews
+
+urlpatterns = [
+    path('', bookviews.home_view, name='book-home'),
+    path('about', bookviews.about_view, name='book-about'),
+]
