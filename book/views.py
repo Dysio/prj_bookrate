a177c45313ff32b9553240e3aabb5e17c65d62d3
Dysio <td.dyszewski@gmail.com>
2020-08-01 22:20:56 +0200

    Update of html templates and making joint with database to display books at home.html

--- book/views.py
+++ book/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
 def home_view(request):
-    return render(request, 'book/home.html')
+    context = {
+        'books':Book.objects.all()
+    }
+    return render(request, 'book/home.html', context)
 
 def about_view(request):
     return render(request, 'book/about.html',{"title":"About"})
