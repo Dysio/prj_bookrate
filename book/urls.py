46f8b9a374e5a860570fd7f8c8941c9d035fb666
Dysio <td.dyszewski@gmail.com>
2020-08-03 16:12:24 +0200

    added class BookListView and pagination

--- book/urls.py
+++ book/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import BookDetailView
+from .views import BookListView,BookDetailView
 from . import views as bookviews
 
 urlpatterns = [
-    path('', bookviews.home_view, name='book-home'),
+    path('', BookListView.as_view(), name='book-home'),
     path('book/<pk>/', BookDetailView.as_view(), name='book-detail'),
     path('about/', bookviews.about_view, name='book-about'),
 ]
--- book/views.py
+++ book/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 from book.models import Book
 from .forms import RateForm
 
@@ -10,6 +10,13 @@ def home_view(request):
         'books':Book.objects.all()
     }
     return render(request, 'book/home.html', context)
+
+class BookListView(ListView):
+    model = Book
+    template_name = 'book/home.html'
+    context_object_name = 'books'
+    ordering = ['title']
+    paginate_by = 5
 
 class BookDetailView(DetailView):
     model = Book
